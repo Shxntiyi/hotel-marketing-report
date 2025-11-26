7b1b21d287d20a781dd485ed97ba40e5cfaaf492
Shxntiyi <chantiyi2802@gmail.com>
2025-11-26 14:30:23 -0500

    chore(app): configurar puerto dinamico para despliegue

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitar CORS para que no te bloquee peticiones externas
+  app.enableCors();
+
+  // OBTENER EL PUERTO DE LA NUBE O USAR 3000 POR DEFECTO
+  const port = process.env.PORT || 3000;
+  
+  // ESCUCHAR EN 0.0.0.0 (Vital para Render/Railway)
+  await app.listen(port, '0.0.0.0');
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
