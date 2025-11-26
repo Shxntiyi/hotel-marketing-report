0cda59487c1f66f3344ba1b65ea893927411a267
Shxntiyi <chantiyi2802@gmail.com>
2025-11-26 14:21:32 -0500

    feat(front): Se agrega una vista para descarga los informes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { AuthModule } from './modules/auth/auth.module';
 import { CloudbedsModule } from './modules/cloudbeds/cloudbeds.module';
 import { ReportsModule } from './modules/reports/reports.module';
 import { MailModule } from './modules/mail/mail.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // Carpeta donde guardaremos el HTML
+    }),
     ConfigModule.forRoot({
       isGlobal: true, 
     }),
